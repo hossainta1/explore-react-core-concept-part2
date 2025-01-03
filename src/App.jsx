902aee3cde70231f6fc5de47a41dc4951520772a
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-01-03 20:15:15 +0600

    Event Handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@
 import './App.css'
 
 function App() {
+  function handleClick(){
+    alert("Button Click")
+  }
+
+  const adFive = (num1) => {
+    alert(num1 + 5)
+  }
  
 
   return (
     <>
-      
+      <h1>React Core Concept part 2</h1>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => adFive(5)}>Add Five</button>
     </>
   )
 }
